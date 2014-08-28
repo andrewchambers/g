f9a157af0f9b42e0a44f26e3ed59ea222c2f7c1a
Andrew Chambers <andrewc@makonetworks.com>
2014-08-28 13:19:12 +1200

    work on lexer

--- parse/lex.go
+++ parse/lex.go
@@ -1,1 +1,51 @@
 package parse
+
+import (
+    "io"
+)
+
+type lexer struct {
+    // Marked token start position in current file.
+    markedPos *FilePos
+    // The io source the lexer is reading for.
+    brdr *bufio.Reader
+    // Current position information
+    curLine int
+    curCol int
+    // Path of current file (used for errors and other position info).
+    path string
+    // Output only channel for sending tokens.
+    stream chan *Token
+}
+
+// Lexers the reader in a goroutine.
+// The goroutine must be read until completion or error.
+// Lexing errors will call onError.
+func Lex(path string, r io.Reader, onError func (error,FilePos)) chan *Token {
+    
+}
+
+// Saves the current lexer position in markedPos.
+func (l *Lexer) mark() {
+    l.markedPos = l.currentPos()
+}
+
+func (l *Lexer) currentPos() *FilePos {
+    return &FilePos{l.path,l.curLine,l.curCol}
+}
+
+// Saves the current lexer position in markedPos.
+func (l *Lexer) sendTok(k TokenKind,val string) {
+    start := l.markedPos
+    end := l.currentPos()
+    span := &FileSpan{start,end}
+    return &Token{k,val,span}
+}
+
+
+
+func (l *Lexer) lex() {
+    for {
+    
+    }
+}
--- parse/token.go
+++ parse/token.go
@@ -2,16 +2,29 @@ package parse
 
 type TokenKind int
 
+
+// The type representing a G lexical token.
 type Token struct {
+    // Tag for the token kind.
     Kind TokenKind
+    // The raw contents of the token.
+    Val string
+    // The file span of the token.
     Span FileSpan
 }
 
+// Represents a single point in the file as shown in most text editors.
+// Tabs are treated as being aligned to 4 places.
 type FilePos struct {
+    //Path 
     Path string
-    Pos int
+    // Line number starting at one.
+    Line int
+    // Col starting at one.
+    Col int
 }
 
+// A span across two points.
 type FileSpan struct {
     Start FilePos
     End FilePos
