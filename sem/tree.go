f76fa4c4964123a395290f8e721ffb6e1e3d6985
andrew chambers <andrewchamberss@gmail.com>
2014-09-02 15:00:40 +1200

    forgot to add tree

---
+++ sem/tree.go
@@ -0,0 +1,36 @@
+package sem
+
+//A function AST is converted into a type checked tree.
+
+type TypeProvider struct {
+	Type GType
+}
+
+func (tp *TypeProvider) GetType() GType {
+	return tp.Type
+}
+
+type SemNode interface {
+	GetType() GType
+}
+
+type SemFunction struct {
+	Statements []SemNode
+}
+
+type SemReturn struct {
+	Expr SemNode
+}
+
+type SemGCall struct {
+	TypeProvider
+	Label string
+	args  []SemNode
+}
+
+type SemCall struct {
+	TypeProvider
+}
+
+type SemString struct {
+}
