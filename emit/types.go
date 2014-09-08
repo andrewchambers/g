05e570a7c0a7730ccb71be6dc819ce93f0102770
Andrew Chambers <andrewc@makonetworks.com>
2014-09-08 16:28:13 +1200

    some dumb code llvm emitting

---
+++ emit/types.go
@@ -0,0 +1,38 @@
+package emit
+
+type GType interface {
+}
+
+type GAlias struct {
+	Name   string
+	Actual GType
+}
+
+type GStruct struct {
+	Names []string
+	Types []GType
+}
+
+type GInt struct {
+	Bits   uint
+	Signed bool
+}
+
+func NewGInt(bits uint,signed bool) *GInt {
+    return &GInt{bits,signed}
+}
+
+
+type GPointer struct {
+	PointsTo GType
+}
+
+type GFunc struct {
+	RetType  GType
+	ArgTypes []GType
+}
+
+type GArray struct {
+	Dim     int64
+	ArrayOf GType
+}
