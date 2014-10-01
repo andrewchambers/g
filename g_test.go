2aa170935fff104196dc90bc99120ee89b0a1c77
Andrew Chambers <andrewc@makonetworks.com>
2014-10-01 15:37:24 +1300

    add broken g_test.go

---
+++ g_test.go
@@ -0,0 +1,37 @@
+package main
+
+// Test the compiler with the standard test tools. This lets us get coverage,
+// profiling, and other nice things.
+
+
+import (
+    "testing"
+    "io/ioutil"
+)
+
+
+
+// Compile a Single file to an llvm text file, returns the result to a channel.
+
+func compileSingleFileToLLVM() {
+
+}
+
+func compileSingleFileToBinary() {
+
+}
+
+func assembleAndLink() {
+
+}
+
+func TestSingleFileRetZero(t *testing.T) {
+    files,err := ioutil.ReadDir("")
+    if err != nil {
+        t.Fatal("failed to read directory containing single file retzero tests.")
+        return
+    }
+    for _,_ = range files {
+        
+    }
+}
